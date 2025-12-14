074f43eb1dc35781ccaa38bb244308404a534d90
Harsheyy <harshkdesai109@yahoo.com>
2025-12-13 20:45:39 -0500

    feat: Add Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -23,6 +24,7 @@ export default function App({ Component, pageProps }) {
         }
       `}</style>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
