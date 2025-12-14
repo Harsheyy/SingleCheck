3adfb166157e34331db02b32ca8af5a149e06b3b
Harsheyy <harshkdesai109@yahoo.com>
2025-12-13 20:51:40 -0500

    fix: Switch Vercel Analytics import to next entrypoint

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }) {
   return (
