98c7d1b97081a4a8954b887f843487f21d3d98f4
Harsheyy <harshkdesai109@yahoo.com>
2025-12-13 20:46:53 -0500

    fix: Update Vercel Analytics import path to react

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
